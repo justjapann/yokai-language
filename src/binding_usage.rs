915c212689351935011a97c32908064b843f7444
justjapann <eduardobessadesouza@gmail.com>
2022-10-21 17:56:40 -0300

    update: binding usage, added some texts and Clone in Val

--- src/binding_usage.rs
+++ src/binding_usage.rs
@@ -1,4 +1,6 @@
+use crate::env::Env;
 use crate::utils;
+use crate::val::Val;
 
 #[derive(Debug, PartialEq)]
 pub struct BindingUsage {
@@ -15,6 +17,10 @@ impl BindingUsage {
                 name: name.to_string(),
             },
         ))
+    }
+
+    pub(crate) fn eval(&self, env: &Env) -> Result<Val, String> {
+        env.get_binding_value(&self.name)
     }
 }
 
@@ -33,6 +39,33 @@ mod tests {
                     name: "abc".to_string(),
                 },
             )),
+        );
+    }
+
+    #[test]
+    fn eval_existing_binding_usage() {
+        let mut env = Env::default();
+        env.store_binding("foo".to_string(), Val::Number(10));
+
+        assert_eq!(
+            BindingUsage {
+                name: "foo".to_string(),
+            }
+            .eval(&env),
+            Ok(Val::Number(10)),
+        );
+    }
+
+    #[test]
+    fn eval_non_existent_binding_usage() {
+        let empty_env = Env::default();
+
+        assert_eq!(
+            BindingUsage {
+                name: "i_dont_exist".to_string(),
+            }
+            .eval(&empty_env),
+            Err("binding with name ‘i_dont_exist’ does not exist".to_string()),
         );
     }
 }
